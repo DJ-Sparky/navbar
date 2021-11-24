e9b09bbaaa2c125ca9c430e14034d4b02d3e68c9
Chris Odegard <c.odegard@gmail.com>
2021-11-24 16:31:36 -0500

    wrap pages with NavBar

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// custom CSS styles
+import "./src/style.css";
+
+import NavBar from './src/pages/Navbar';
+
+export const wrapPageElement = ({ element, props }) => (<div><header><NavBar/></header>{element}</div>)
+
