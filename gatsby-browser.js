9a5963e12d805c5c21eed3cd80b591e20ce7ad06
Chris Odegard <c.odegard@gmail.com>
2021-11-24 18:16:53 -0500

    pass props (like location) into NavBar

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,11 @@ import "./src/style.css";
 
 import NavBar from './src/pages/Navbar';
 
-export const wrapPageElement = ({ element, props }) => (<div><header><NavBar/></header>{element}</div>)
+export const wrapPageElement = ({ element, props }) => (
+  <div>
+    <header>
+      <NavBar {...props} />
+    </header>
+    {element}
+  </div>)
 
