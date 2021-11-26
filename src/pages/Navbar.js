72ea2c3da99e1ecc757bb1e04cc63b06623b16f4
popbang <popbang@mail.com>
2021-11-26 12:51:42 +0000

    Update all

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'gatsby';
 
-const NavBar = ({location}) => {
-  // Create a hook to toggle the state variable
-  const [isNavVisible, toggleNavVisibility] = useState(false);
-  useEffect(() => {
-    console.log(`effect! ${location.pathname}`);
-    toggleNavVisibility(false);
-  }, [location]);
+const NavBar = ({ location }) => {
+	// Create a hook to toggle the state variable
+	const [isNavVisible, toggleNavVisibility] = useState(false);
+	useEffect(() => {
+		console.log(`effect! ${location.pathname}`);
+		toggleNavVisibility(false);
+	}, [location]);
 
-<<<<<<< HEAD
 	return (
 		<nav id='navbar' className='navbar-container container'>
 			<Link to='/' className='home-link'>
@@ -73,54 +72,5 @@ const NavBar = ({location}) => {
 			)}
 		</nav>
 	);
-=======
-  return (
-    <nav id="navbar" className='navbar-container container'>
-      <Link to='/' className='home-link' state={{ fromNav: true }}>
-        Website Name
-      </Link>
-      <button
-        type="button"
-        role="button"
-        id='navbar-toggle'
-        aria-controls='navbar-menu'
-        aria-label='Toggle menu'
-        aria-expanded={isNavVisible}
-        onClick={e => toggleNavVisibility(!isNavVisible)}
-      >
-        <span className='icon-bar'></span>
-        <span className='icon-bar'></span>
-        <span className='icon-bar'></span>
-      </button>
-      {/* // Only show the menu if isNavVisible === true */}
-      {isNavVisible && (
-        <div id='navbar-menu' aria-labelledby='navbar-toggle'>
-          <ul className='navbar-links'>
-            <li className='navbar-item'>
-              <Link className='navbar-link' to='/about'>
-                About
-              </Link>
-            </li>
-            <li className='navbar-item'>
-              <Link className='navbar-link' to='/blog'>
-                Blog
-              </Link>
-            </li>
-            <li className='navbar-item'>
-              <Link className='navbar-link' to='/careers'>
-                Careers
-              </Link>
-            </li>
-            <li className='navbar-item'>
-              <Link className='navbar-link' to='/contact'>
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-      )}
-    </nav>
-  );
->>>>>>> 612c5672a5c0f232c8d7ea1074cb9844c25f0559
 };
 export default NavBar;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from 'react';
 
 // styles
@@ -23,51 +22,3 @@ const ContactPage = () => {
 };
 
 export default ContactPage;
-=======
-import * as React from "react"
-import { Link } from "gatsby"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const AboutPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Contact</title>
-      <h1 style={headingStyles}>Contact</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
-
-export default AboutPage
->>>>>>> 612c5672a5c0f232c8d7ea1074cb9844c25f0559
